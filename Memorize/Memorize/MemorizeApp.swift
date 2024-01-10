0d892bbe1b72904fa9dd9a020ed2b479ca951f69
radcli14 <radcli14@gmail.com>
2024-01-10 17:56:34 +0100

    Initial commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Eliott Radcliffe on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
