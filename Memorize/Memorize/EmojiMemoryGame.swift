cbd09afac6ce128cba37d6aaa919e9194082d193
radcli14 <radcli14@gmail.com>
2024-01-11 16:20:29 +0100

    began implementation of model and viewmodel from lecture 3

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+import SwiftUI
+
+class EmojiMemoryGame {
+    var model: MemoryGame<String>
+}
--- Memorize/Memorize/MemorizeGame.swift
+++ Memorize/Memorize/MemorizeGame.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    func chooseCard(card: Card) {
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+}
