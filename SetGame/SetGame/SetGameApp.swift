1e97b97d9426279c27d857f88611a33e0fb50af1
radcli14 <radcli14@gmail.com>
2024-01-20 12:54:03 +0100

    added the game of Set for programming assignment 3

---
+++ SetGame/SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Eliott Radcliffe on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
